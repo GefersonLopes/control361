63cb6c92e59d5ebe326bbcf93b87b4dd7438560b
Geferson Lopes <gefersonjefreey@gmail.com>
2025-05-08 07:51:53 -0300

    create projetc and initial config to use

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default [
+  tseslint.config(
+    { ignores: ["dist"] },
+    {
+      extends: [js.configs.recommended, ...tseslint.configs.recommended],
+      files: ["**/*.{ts,tsx}"],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+      },
+      plugins: {
+        "react-hooks": reactHooks,
+        "react-refresh": reactRefresh,
+      },
+      rules: {
+        ...reactHooks.configs.recommended.rules,
+        "react-refresh/only-export-components": [
+          "warn",
+          { allowConstantExport: true },
+        ],
+      },
+    },
+  ),
+
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "plugin:@typescript-eslint/recommended",
+      "eslint-config-prettier",
+    ],
+  }),
+
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "unused-imports/no-unused-imports": "error",
+
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+  },
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+];
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./global.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-500 bg-black">Control 361</h1>
+    </div>
+  );
+}
+
+export default App;
