a327bd8d6ab0d6a53efe1869d4afe811a3fa377a
Geferson Lopes <gefersonjefreey@gmail.com>
2025-05-08 21:28:50 -0300

    feat: mock veicles

---
+++ src/types/veicle.ts
@@ -0,0 +1,8 @@
+export interface Vehicle {
+  id: number;
+  pos: [number, number];
+  placa: string;
+  color: string;
+  frota: string;
+  data: Date;
+}
---
+++ src/utils/data/veicles.ts
@@ -0,0 +1,30 @@
+import type { Vehicle } from "../../types/veicle";
+
+const vehicles: Vehicle[] = [
+  {
+    id: 1,
+    pos: [-23.56, -46.64],
+    placa: "PRO 8C2 174",
+    color: "#e11d48",
+    frota: "000001",
+    data: new Date(),
+  },
+  {
+    id: 2,
+    pos: [-23.55, -46.62],
+    placa: "KER 7D3 981",
+    color: "#3b82f6",
+    frota: "000002",
+    data: new Date(),
+  },
+  {
+    id: 3,
+    pos: [-23.54, -46.65],
+    placa: "PCK 9H1 222",
+    color: "#10b981",
+    frota: "000003",
+    data: new Date(),
+  },
+];
+
+export default vehicles;
