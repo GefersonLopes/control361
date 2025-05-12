cef1bd3d9c82306dddfb2b578477ff4d2defc758
Geferson Lopes <gefersonjefreey@gmail.com>
2025-05-11 21:47:22 -0300

    fix: config to use test e2e and jest

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,18 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents() {
-      // implement node event listeners here
+    baseUrl: "http://localhost:5173",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/index.ts",
+    setupNodeEvents(on, config) {
+      return config;
     },
+  },
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    specPattern: "cypress/e2e/**/*.cy.{ts,tsx}",
   },
 });
