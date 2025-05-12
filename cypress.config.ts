bbb5528e276d6fe2c2743f98c8cd16134c341539
Geferson Lopes <gefersonjefreey@gmail.com>
2025-05-12 00:15:55 -0300

    fix: config cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    supportFile: "cypress/support/index.ts",
+    supportFile: "cypress/support/e2e.ts",
     setupNodeEvents(on, config) {
       return config;
     },
